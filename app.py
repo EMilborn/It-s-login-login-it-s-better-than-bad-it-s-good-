da70a3f73d1b0fac1e731ed8cde0f0e57a7deb31
Elias Milborn <emilborn@stuy.edu>
2016-10-04 22:53:00 -0400

    stil have issues, doesn't recognize module, don't know how to csv

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from login import register, login
+import hashlib
 
 app = Flask(__name__)
 
@@ -8,13 +10,35 @@ def getForm():
     return render_template('form.html',
                            name01 = "Username",
                            name02 = "Password",
-                           heading01 = "LOGIN")
+                           heading01 = "LOGIN",
+                           name03 = "R/L",
+                           name04 = "Register",
+                           name05 = "Login")
 
 @app.route("/authenticate/", methods = ['POST'])
 def auth():
-    if request.form["Username"] == "Dyrland" and request.form["Password"] == "Weaver":
-        return "Hi Mr. DW!!!"
-    return "Who are you??"
+    winOrLose = "ERROR"
+    password = hashlib.sha256(request.form["Password"]).hexdigest()
+    username = request.form("Username")
+    
+    if request.form["R/L"] == "Register":
+        if login.register(username, password):
+            winOrLose = "Register Successful"
+        else:
+            winOrLose = "Register Failed"
+
+    elif request.form["R/L"] == "Login":
+        if login.login(username, password):
+            winOrLose = "Login Successful"
+        else:
+            winOrLose = "Login Failed"
+
+    return render_template('authenticate.html',
+                           text = winOrLose)
+            
+    
+        
+    
 
 if __name__ == "__main__":
     #enable debugging, auto-restarting of server when this file is modified
